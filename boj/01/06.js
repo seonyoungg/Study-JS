9a04ca21c748a725c722f60257cf6d0ebb59b8d8
seonyoungg <tjsduddlfma1@naver.com>
2025-03-30 18:52:06 +0900

    ✏️ study : boj practice

--- boj/01/06.js
+++ boj/01/06.js
@@ -22,4 +22,10 @@
 2
 1
 */
+const fs = require('fs');
+const fileData = fs.readFileSync(0).toString().trim().split(' ');
+// // console.log(fileData);
 
+const a = parseInt(fileData[0]);
+const b = parseInt(fileData[1]);
+console.log(`${a + b}\n${a - b}\n${a * b}\n${Math.trunc(a / b)}\n${a % b}`);
--- boj/01/10.js
+++ boj/01/10.js
@@ -34,3 +34,11 @@
 181720
 */
 
+const fs = require('fs');
+const fileData = fs.readFileSync(0).toString().trim().split(' ');
+// console.log(fileData);
+
+const a = parseInt(fileData[0]);
+const b = parseInt(fileData[1]);
+
+console.log(a + b);
